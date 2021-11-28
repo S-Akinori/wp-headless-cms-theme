733eb2a11be8b10c3337e9bba6cdf39e23f3d86f
Akinori <aki10.working@gmail.com>
2021-11-28 18:14:27 +0900

    add thumbnail & preview

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * activate thumbnail
+ */
+add_theme_support( 'post-thumbnails' );
 
 /**
  * deploy to vercel when a post is created or updated
@@ -7,14 +11,36 @@
  */
 function deploy_to_vercel() {
   $url = ''; // set Vercel deploy hook api 
+	if($url) {
+	  $context = array(
+      'http' => array(
+        'method'  => 'POST',
+      )
+	  );
 
-  $context = array(
-    'http' => array(
-      'method'  => 'POST',
-    )
-  );
-
-  $json_res = file_get_contents($url, false, stream_context_create($context));
-  return $json_res;
+	  $json_res = file_get_contents($url, false, stream_context_create($context));
+	  return $json_res;
+	}
+	return false;
 }
 add_action('save_post', 'deploy_to_vercel');
+
+/**
+ *  return wordpress refresh token from GraphQL IDE
+ * 
+*/
+add_filter( 'graphql_jwt_auth_secret_key', function() {
+  $refresh_token = ''; // set refresh token
+  return $refresh_token;
+});
+
+/**
+ * redirect to custom preview link
+*/
+function preview_link($link) {
+	global $post;
+	$secret = ''; // set preview secret
+	$new_link = 'http://localhost:3000/api/preview?secret='.$secret.'&id='.$post->ID;
+   return $new_link;
+}
+add_filter('preview_post_link', 'preview_link');
